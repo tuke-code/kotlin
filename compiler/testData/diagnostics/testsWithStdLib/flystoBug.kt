202dac88e9dab78f8e471467a9353f201adf4254
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2023-11-24 14:57:14 +0100

    ~ Simplification of test

--- compiler/testData/diagnostics/testsWithStdLib/flystoBug.kt
+++ compiler/testData/diagnostics/testsWithStdLib/flystoBug.kt
@@ -1,54 +1,31 @@
 // For PCLA branch
 // FIR_DUMP
 
-class Data<D : Comparable<D>>(val converter: DoubleConverter<D>)
+interface Units<UValue : Any>
 
-interface Units<UValue : Any> : DoubleConverter<UValue> {
-    companion object {
-        val Percent = SimpleDoubleUnits()
-    }
-}
+class SimpleDoubleUnits : Units<Double>
 
-class SimpleDoubleUnits : BaseUnits<Double>()
+fun <BLeft : Any> build(builderCode: RendererBuilder<BLeft>.() -> Unit) {}
 
-abstract class BaseUnits<BValue : Any> : Units<BValue>
-
-interface DoubleConverter<DValue : Any>
-
-interface Renderer<TX : Any, Left : Any, Right : Any> {
-    companion object {
-        fun <BX : Any, BLeft : Any, BRight : Any> build(
-            xConverter: DoubleConverter<BX>,
-            builderCode: RendererBuilder<BX, BLeft, BRight>.() -> Unit
-        ): Renderer<BX, BLeft, BRight> = null!!
-    }
-}
-
-class RendererBuilder<RBX : Any, RBLeft : Any, RBRight : Any> {
+class RendererBuilder<RBLeft : Any> {
     var leftScaleCurves: CurveSet<RBLeft>? = null
 
-
-    fun addDecorations(render: RenderDecorations<RBX, RBLeft, RBRight>) {}
+    fun addDecorations(render: suspend RenderContext<RBLeft>.() -> Unit) {}
 }
 
-typealias RenderDecorations<GDX, GDLeft, GDRight> = suspend RenderContext<GDX, GDLeft, GDRight>.() -> Unit
-
-interface RenderContext<RCX : Any, RCLeft : Any, RCRight : Any> {
+interface RenderContext<RCLeft : Any> {
     val leftScaleValueToY: ((RCLeft) -> Double)?
 }
 
-class State<S : Comparable<S>>(val data: Data<S>) {
+class State {
     suspend fun render() {
-        val renderer = Renderer.build(xConverter = data.converter) {
-            leftScaleCurves = CurveSet(units = Units.Percent)
+        build {
+            leftScaleCurves = CurveSet(SimpleDoubleUnits())
             addDecorations {
-                val top = leftScaleValueToY!!(0.67)
-                renderCurrentValue(top)
+                leftScaleValueToY!!(0.67)
             }
         }
     }
-
-    private fun RenderContext<S, Double, Unit>.renderCurrentValue(d: Double) {}
 }
 
 class CurveSet<CY : Any>(units: Units<CY>)
