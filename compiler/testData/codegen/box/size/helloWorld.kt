bce7b530846bc502eb6b49c0c616f7733b75e01e
Zalim Bashorov <zalim.bashorov@jetbrains.com>
2024-01-12 09:05:08 +0100

    ~ update test data

--- compiler/testData/codegen/box/size/helloWorld.kt
+++ compiler/testData/codegen/box/size/helloWorld.kt
@@ -1,7 +1,7 @@
 // TARGET_BACKEND: WASM
 
 // WASM_DCE_EXPECTED_OUTPUT_SIZE: wasm 40_730
-// WASM_DCE_EXPECTED_OUTPUT_SIZE:  mjs  4_552
+// WASM_DCE_EXPECTED_OUTPUT_SIZE:  mjs  4_637
 
 fun box(): String {
     println("Hello, World!")
