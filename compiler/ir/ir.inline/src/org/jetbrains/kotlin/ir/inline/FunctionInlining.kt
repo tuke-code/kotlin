d75d9e29576f8831c1fba8615003d5b7742fb6f5
Wojciech Litewka <wojciech.litewka@jetbrains.com>
2024-02-13 15:41:29 +0100

    [IR] Push default values from base to implementation classes
    The default value of those properties is a detail that should be handled
     by implementation or builder layer.
    
     This change will also simplify auto-generating and reasoning about
     generated implementation classes, and allow for potential further
     enhancements like intercepting all mutations.
    
    ^KT-65773 In Progress

--- compiler/ir/ir.inline/src/org/jetbrains/kotlin/ir/inline/FunctionInlining.kt
+++ compiler/ir/ir.inline/src/org/jetbrains/kotlin/ir/inline/FunctionInlining.kt
@@ -862,6 +862,9 @@ class FunctionInlining(
                 symbol.owner.type = value
             }
 
+        override var attributeOwnerId: IrAttributeContainer = this
+        override var originalBeforeInline: IrAttributeContainer? = null
+
         override fun <R, D> accept(visitor: IrElementVisitor<R, D>, data: D) =
             visitor.visitGetValue(this, data)
 
