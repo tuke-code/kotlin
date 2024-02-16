f503b3963e39662dcbd56a2cf637b0c01605445d
Wojciech Litewka <wojciech.litewka@jetbrains.com>
2024-02-16 12:42:49 +0100

    [IR] Autogenerate IrFieldImpl
    ^KT-65773 In Progress

--- compiler/ir/ir.tree/tree-generator/src/org/jetbrains/kotlin/ir/generator/ImplementationConfigurator.kt
+++ compiler/ir/ir.tree/tree-generator/src/org/jetbrains/kotlin/ir/generator/ImplementationConfigurator.kt
@@ -67,7 +67,7 @@ object ImplementationConfigurator : AbstractIrTreeImplementationConfigurator() {
         }
 
         impl(field) {
-            implementation.doPrint = false
+            defaultNull("initializer", "correspondingPropertySymbol")
         }
 
         impl(property) {
