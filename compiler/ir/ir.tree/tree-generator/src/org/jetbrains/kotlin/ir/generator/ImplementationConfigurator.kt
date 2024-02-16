50cbf11b1cccbec4a1b0142486100fd097746396
Wojciech Litewka <wojciech.litewka@jetbrains.com>
2024-02-16 12:43:42 +0100

    [IR] Autogenerate IrPropertyImpl
    ^KT-65773 In Progress

--- compiler/ir/ir.tree/tree-generator/src/org/jetbrains/kotlin/ir/generator/ImplementationConfigurator.kt
+++ compiler/ir/ir.tree/tree-generator/src/org/jetbrains/kotlin/ir/generator/ImplementationConfigurator.kt
@@ -71,7 +71,7 @@ object ImplementationConfigurator : AbstractIrTreeImplementationConfigurator() {
         }
 
         impl(property) {
-            implementation.doPrint = false
+            defaultNull("backingField", "getter", "setter")
         }
         impl(propertyWithLateBinding) {
             implementation.doPrint = false
