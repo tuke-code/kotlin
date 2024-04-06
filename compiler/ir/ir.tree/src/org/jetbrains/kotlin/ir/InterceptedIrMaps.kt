a56673096b8911d039ea96158810f4f7d241a2d7
Wojciech Litewka <wojciech.litewka@jetbrains.com>
2024-04-06 12:12:33 +0200

    [IR] Substitute IR maps with dynamic properties

--- compiler/ir/ir.tree/src/org/jetbrains/kotlin/ir/InterceptedIrMaps.kt
+++ compiler/ir/ir.tree/src/org/jetbrains/kotlin/ir/InterceptedIrMaps.kt
@@ -12,21 +12,25 @@ import kotlin.collections.HashSet
 import kotlin.collections.LinkedHashMap
 import kotlin.collections.LinkedHashSet
 
-fun<K : IrElement, V> irIntercepted_HashMap() = HashMap<K, V>()
+fun<K : IrElement, V> irIntercepted_HashMap() = createIrDynamicProperty<K, V>()
 fun<K : IrElement, V> irIntercepted_hashMapOf() = irIntercepted_HashMap<K, V>()
 
-fun<K : IrElement, V> irIntercepted_LinkedHashMap() = LinkedHashMap<K, V>()
+fun<K : IrElement, V> irIntercepted_LinkedHashMap() = createIrDynamicProperty<K, V>()
 
 fun<K : IrElement, V> irIntercepted_MutableMap() = irIntercepted_LinkedHashMap<K, V>()
 
 fun<K : IrElement, V> irInterceptedLocal_HashMap() = HashMap<K, V>()
 fun<K : IrElement, V> irInterceptedLocal_hashMapOf() = irInterceptedLocal_HashMap<K, V>()
 
-fun<K : IrElement, V> irIntercepted_WeakHashMap() = WeakHashMap<K, V>()
+fun<K : IrElement, V> irIntercepted_WeakHashMap() = createIrDynamicProperty<K, V>()
 
-fun<K : IrElement, V> irIntercepted_ConcurrentHashMap() = ConcurrentHashMap<K, V>()
+fun<K : IrElement, V> irIntercepted_ConcurrentHashMap() = createIrDynamicProperty<K, V>()
 
-fun<K : IrElement> irIntercepted_HashSet() = HashSet<K>()
+
+fun<K : IrElement> irIntercepted_HashSet() = createIrDynamicFlag<K>()
+
+fun<K : IrElement> irIntercepted_MutableSet() = createIrDynamicFlag<K>()
+
 fun<K : IrElement> irInterceptedLocal_HashSet() = HashSet<K>()
-fun<K : IrElement> irIntercepted_MutableSet() = LinkedHashSet<K>()
-fun<K : IrElement> irIntercepted_ConcurrentHashSet() = ConcurrentHashMap.newKeySet<K>()
+
+fun<K : IrElement> irIntercepted_ConcurrentHashSet() = createIrDynamicFlag<K>()
