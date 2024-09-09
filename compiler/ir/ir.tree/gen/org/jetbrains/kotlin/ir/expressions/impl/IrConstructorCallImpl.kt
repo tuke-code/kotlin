a284a3f24a322acec479534b49e3c7137ae4597e
Wojciech Litewka <wojciech.litewka@jetbrains.com>
2024-09-09 14:35:32 +0200

    [IR] Drop IrFunctionAccessExpression.contextReceiversCount
    It was unused in practice.
    There will be a similar property introduced for the purpose of migration
    from old to new argument access API.
    
    KT-70054

--- compiler/ir/ir.tree/gen/org/jetbrains/kotlin/ir/expressions/impl/IrConstructorCallImpl.kt
+++ compiler/ir/ir.tree/gen/org/jetbrains/kotlin/ir/expressions/impl/IrConstructorCallImpl.kt
@@ -41,7 +41,6 @@ class IrConstructorCallImpl internal constructor(
 
     override var extensionReceiver: IrExpression? = null
 
-    override var contextReceiversCount: Int = 0
 
     companion object
 }
