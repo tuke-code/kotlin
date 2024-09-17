d2857695d4340a126be8ec849643ea22e4eee251
Wojciech Litewka <wojciech.litewka@jetbrains.com>
2024-09-17 14:37:45 +0200

    [IR] Pull implementation of parameter data to IrFunction class

--- compiler/ir/ir.tree/tree-generator/src/org/jetbrains/kotlin/ir/generator/ImplementationConfigurator.kt
+++ compiler/ir/ir.tree/tree-generator/src/org/jetbrains/kotlin/ir/generator/ImplementationConfigurator.kt
@@ -61,8 +61,7 @@ object ImplementationConfigurator : AbstractIrTreeImplementationConfigurator() {
         }
 
         allImplOf(function) {
-            defaultNull("dispatchReceiverParameter", "extensionReceiverParameter", "body")
-            default("contextReceiverParametersCount", "0")
+            defaultNull("body")
             isLateinit("returnType")
         }
 
