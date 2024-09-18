b39804dfd0e66b5e8c5765ed704bf6bdde66e8a4
Maria Sokolova <maria.sokolova@jetbrains.com>
2024-09-18 18:40:26 +0200

    fixup! Added asJavaAtomicArray and asKotlinAtomicArray extensions.

--- libraries/stdlib/jvm/src/kotlin/concurrent/AtomicArrays.jvm.kt
+++ libraries/stdlib/jvm/src/kotlin/concurrent/AtomicArrays.jvm.kt
@@ -5,6 +5,8 @@
 
 package kotlin.concurrent
 
+import java.util.concurrent.atomic.*
+
 public actual class AtomicIntArray public actual constructor(public actual val size: Int) {
     public actual constructor(array: IntArray) : this(array.size)
 
@@ -32,6 +34,18 @@ public actual class AtomicIntArray public actual constructor(public actual val s
 
     public actual override fun toString(): String = TODO()
 }
+
+/**
+ * Casts the given [AtomicIntArray] instance to [java.util.concurrent.atomic.AtomicIntegerArray].
+ */
+@Suppress("UNCHECKED_CAST")
+public fun AtomicIntArray.asJavaAtomicArray(): AtomicIntegerArray = this as AtomicIntegerArray
+
+/**
+ * Casts the given [java.util.concurrent.atomic.AtomicIntegerArray] instance to [AtomicIntArray].
+ */
+@Suppress("UNCHECKED_CAST")
+public fun AtomicIntegerArray.asKotlinAtomicArray(): AtomicIntArray = this as AtomicIntArray
 
 public actual class AtomicLongArray public actual constructor(public actual val size: Int) {
 
@@ -62,6 +76,18 @@ public actual class AtomicLongArray public actual constructor(public actual val 
     public actual override fun toString(): String = TODO()
 }
 
+/**
+ * Casts the given [AtomicLongArray] instance to [java.util.concurrent.atomic.AtomicLongArray].
+ */
+@Suppress("UNCHECKED_CAST")
+public fun AtomicLongArray.asJavaAtomicArray(): java.util.concurrent.atomic.AtomicLongArray = this as java.util.concurrent.atomic.AtomicLongArray
+
+/**
+ * Casts the given [java.util.concurrent.atomic.AtomicLongArray] instance to [AtomicLongArray].
+ */
+@Suppress("UNCHECKED_CAST")
+public fun java.util.concurrent.atomic.AtomicLongArray.asKotlinAtomicArray(): AtomicLongArray = this as AtomicLongArray
+
 public actual class AtomicArray<T> {
     private val array: Array<T>
 
@@ -83,3 +109,15 @@ public actual class AtomicArray<T> {
 
     public actual override fun toString(): String = TODO()
 }
+
+/**
+ * Casts the given [AtomicArray]<T> instance to [java.util.concurrent.atomic.AtomicReferenceArray]<T>.
+ */
+@Suppress("UNCHECKED_CAST")
+public fun <T> AtomicArray<T>.asJavaAtomicArray(): AtomicReferenceArray<T> = this as AtomicReferenceArray<T>
+
+/**
+ * Casts the given [java.util.concurrent.atomic.AtomicReferenceArray]<T> instance to [AtomicArray]<T>.
+ */
+@Suppress("UNCHECKED_CAST")
+public fun <T> AtomicReferenceArray<T>.asKotlinAtomicArray(): AtomicArray<T> = this as AtomicArray<T>
