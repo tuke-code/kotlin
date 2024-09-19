0cff5b99ca7c963f1fa9ca74a2fe9704f0748d15
Maria Sokolova <maria.sokolova@jetbrains.com>
2024-09-19 14:43:32 +0200

    [stdlib] Added Atomic and AtomicArray builtin classes with corresponding codegen mappings for a bootstrap compiler.

--- libraries/stdlib/build.gradle.kts
+++ libraries/stdlib/build.gradle.kts
@@ -423,6 +423,7 @@ kotlin {
                 val unimplementedNativeBuiltIns =
                     (file(jvmBuiltinsDir).list()!!.toSortedSet() - file("$jsDir/builtins/").list()!!)
                         .map { "$jvmBuiltinsRelativeDir/$it" }
+                        .filterNot { it == "$jvmBuiltinsRelativeDir/Atomics.jvm.kt" || it == "$jvmBuiltinsRelativeDir/AtomicArrays.jvm.kt" }
 
                 val sources = unimplementedNativeBuiltIns
 
@@ -487,7 +488,7 @@ kotlin {
 
                 val excluded = listOf(
                     // Included with K/N collections
-                    "Collections.kt", "Iterator.kt"
+                    "Collections.kt", "Iterator.kt", "Atomics.jvm.kt", "AtomicArrays.jvm.kt"
                 )
 
                 sources.forEach { path ->
