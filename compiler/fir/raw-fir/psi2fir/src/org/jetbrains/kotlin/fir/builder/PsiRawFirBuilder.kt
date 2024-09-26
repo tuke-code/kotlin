710ae28092a3340eb42728cba50fddbd20a7a944
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2024-09-26 13:22:41 +0200

    K2 REPL: fix raw fir building

--- compiler/fir/raw-fir/psi2fir/src/org/jetbrains/kotlin/fir/builder/PsiRawFirBuilder.kt
+++ compiler/fir/raw-fir/psi2fir/src/org/jetbrains/kotlin/fir/builder/PsiRawFirBuilder.kt
@@ -1403,42 +1403,40 @@ open class PsiRawFirBuilder(
                 symbol = snippetSymbol
 
                 withContainerSymbol(snippetSymbol, isLocal = true) {
-                    buildBlock {
-                        script.declarations.forEach { declaration ->
-                            when (declaration) {
-                                is KtScriptInitializer -> {
-                                    val initializer = buildAnonymousInitializer(
-                                        initializer = declaration,
-                                        containingDeclarationSymbol = snippetSymbol,
-                                        allowLazyBody = true,
-                                        isLocal = true,
-                                    )
+                    script.declarations.forEach { declaration ->
+                        when (declaration) {
+                            is KtScriptInitializer -> {
+                                val initializer = buildAnonymousInitializer(
+                                    initializer = declaration,
+                                    containingDeclarationSymbol = snippetSymbol,
+                                    allowLazyBody = true,
+                                    isLocal = true,
+                                )
 
-                                    statements.addAll(initializer.body!!.statements)
-                                }
-                                is KtDestructuringDeclaration -> {
-                                    val destructuringContainerVar = buildScriptDestructuringDeclaration(declaration)
-                                    statements.add(destructuringContainerVar)
+                                statements.addAll(initializer.body!!.statements)
+                            }
+                            is KtDestructuringDeclaration -> {
+                                val destructuringContainerVar = buildScriptDestructuringDeclaration(declaration)
+                                statements.add(destructuringContainerVar)
 
-                                    addDestructuringVariables(
-                                        statements,
-                                        this@Visitor,
-                                        baseModuleData,
-                                        declaration,
-                                        destructuringContainerVar,
-                                        tmpVariable = false,
-                                        forceLocal = false,
-                                    ) {
-                                        configureScriptDestructuringDeclarationEntry(it, destructuringContainerVar)
-                                    }
+                                addDestructuringVariables(
+                                    statements,
+                                    this@Visitor,
+                                    baseModuleData,
+                                    declaration,
+                                    destructuringContainerVar,
+                                    tmpVariable = false,
+                                    forceLocal = false,
+                                ) {
+                                    configureScriptDestructuringDeclarationEntry(it, destructuringContainerVar)
                                 }
-                                else -> {
-                                    val firStatement = declaration.toFirStatement()
-                                    if (firStatement is FirDeclaration) {
-                                        statements.add(firStatement)
-                                    } else {
-                                        error("unexpected declaration type in script")
-                                    }
+                            }
+                            else -> {
+                                val firStatement = declaration.toFirStatement()
+                                if (firStatement is FirDeclaration) {
+                                    statements.add(firStatement)
+                                } else {
+                                    error("unexpected declaration type in script")
                                 }
                             }
                         }
