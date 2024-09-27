a9e1c07bd9d110bb0935e4af41e5e468f627ec43
Marco Pennekamp <marco.pennekamp@jetbrains.com>
2024-09-27 11:52:30 +0200

    [FIR] Add `FirCachesFactory.createCacheWithSuggestedLimits`
    - `createCacheWithSuggestedLimits` creates a cache which is potentially
      limited in dimensions such as time, size, and reference strength. Such
      a cache can be used to limit the accumulation of garbage and thus save
      memory.
    - The suggested limits are applied or ignored based on the cache
      factory: The compiler cache factory constructs an unlimited cache,
      while the Analysis API cache factory honors these limits. This allows
      the Analysis API to benefit from a limited cache, while the compiler
      cache structure remains unchanged.
    
    ^KT-70701

--- analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/caches/FirThreadSafeCachesFactory.kt
+++ analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/caches/FirThreadSafeCachesFactory.kt
@@ -5,10 +5,14 @@
 
 package org.jetbrains.kotlin.analysis.low.level.api.fir.fir.caches
 
+import com.github.benmanes.caffeine.cache.Cache
+import com.github.benmanes.caffeine.cache.Caffeine
 import org.jetbrains.kotlin.fir.caches.FirCache
 import org.jetbrains.kotlin.fir.caches.FirCachesFactory
 import java.util.concurrent.ConcurrentHashMap
 import org.jetbrains.kotlin.fir.caches.FirLazyValue
+import kotlin.time.Duration
+import kotlin.time.toJavaDuration
 
 object FirThreadSafeCachesFactory : FirCachesFactory() {
     override fun <KEY : Any, VALUE, CONTEXT> createCache(createValue: (KEY, CONTEXT) -> VALUE): FirCache<KEY, VALUE, CONTEXT> =
@@ -31,6 +35,57 @@ object FirThreadSafeCachesFactory : FirCachesFactory() {
     ): FirCache<KEY, VALUE, CONTEXT> =
         FirThreadSafeCacheWithPostCompute(createValue, postCompute)
 
+    override fun <K : Any, V, CONTEXT> createCacheWithSuggestedLimits(
+        expirationAfterAccess: Duration?,
+        maximumSize: Long?,
+        keyStrength: KeyReferenceStrength,
+        valueStrength: ValueReferenceStrength,
+        createValue: (K, CONTEXT) -> V
+    ): FirCache<K, V, CONTEXT> {
+        if (
+            expirationAfterAccess == null &&
+            maximumSize == null &&
+            keyStrength == KeyReferenceStrength.STRONG &&
+            valueStrength == ValueReferenceStrength.STRONG
+        ) {
+            return createCache(createValue)
+        }
+
+        val builder = Caffeine<K, V>.newBuilder()
+
+        if (expirationAfterAccess != null) {
+            builder.expireAfterAccess(expirationAfterAccess.toJavaDuration())
+        }
+
+        if (maximumSize != null) {
+            builder.maximumSize(maximumSize.toLong())
+        }
+
+        if (keyStrength == KeyReferenceStrength.WEAK) {
+            builder.weakKeys()
+        }
+
+        when (valueStrength) {
+            ValueReferenceStrength.STRONG -> {}
+            ValueReferenceStrength.SOFT -> builder.softValues()
+            ValueReferenceStrength.WEAK -> builder.weakValues()
+        }
+
+        return FirCaffeineCache(builder.build(), createValue)
+    }
+
     override fun <V> createLazyValue(createValue: () -> V): FirLazyValue<V> =
         FirThreadSafeValue(createValue)
+}
+
+private class FirCaffeineCache<K : Any, V, CONTEXT>(
+    private val cache: Cache<K, Any>,
+    private val createValue: (K, CONTEXT) -> V,
+) : FirCache<K, V, CONTEXT>() {
+    override fun getValue(key: K, context: CONTEXT): V {
+        @Suppress("UNCHECKED_CAST")
+        return cache.get(key) { k -> createValue(k, context) ?: NullValue }?.nullValueToNull() as V
+    }
+
+    override fun getValueIfComputed(key: K): V? = cache.getIfPresent(key)?.nullValueToNull()
 }
