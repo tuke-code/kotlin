433e53f9b945c2f7a992b8a5a43b36f1dc79ce8e
Sarah Haggarty <Sarah.Haggarty@jetbrains.com>
2024-10-10 08:39:07 +0000

    fixup! Added examples for the rest functions

--- libraries/tools/kotlin-gradle-plugin-api/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/CinteropSettings.kt
+++ libraries/tools/kotlin-gradle-plugin-api/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/CinteropSettings.kt
@@ -130,7 +130,7 @@ interface CInteropSettings : Named {
          *     }
          * }
          * ```
-         * In the example above, the directory `include/libs` will be specified as prefix for the listed in the `headerFilter`
+         * In the example, the directory `include/libs` is specified as the prefix for the directories listed in the `headerFilter`
          * declared in the `custom.def` option.
          *
          * @param includeDirs The directories to be included as a prefixes for the header filters.
