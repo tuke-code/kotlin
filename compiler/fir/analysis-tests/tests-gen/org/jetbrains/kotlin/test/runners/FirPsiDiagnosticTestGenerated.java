8ba41e7c9b988814728583b308a937f82d24d7c8
Alejandro Serrano Mena <alejandro.serrano.mena@jetbrains.com>
2024-10-14 17:15:07 +0200

    Argument resolution kind of works

--- compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/FirPsiDiagnosticTestGenerated.java
+++ compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/FirPsiDiagnosticTestGenerated.java
@@ -5364,6 +5364,12 @@ public class FirPsiDiagnosticTestGenerated extends AbstractFirPsiDiagnosticTest 
       }
 
       @Test
+      @TestMetadata("arguments.kt")
+      public void testArguments() {
+        runTest("compiler/fir/analysis-tests/testData/resolve/unqualifiedEnum/arguments.kt");
+      }
+
+      @Test
       @TestMetadata("correctJava.kt")
       public void testCorrectJava() {
         runTest("compiler/fir/analysis-tests/testData/resolve/unqualifiedEnum/correctJava.kt");
