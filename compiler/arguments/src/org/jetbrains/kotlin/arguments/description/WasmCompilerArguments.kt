fe94dda844ba6568eb79cdc24ac9a181de37b3b1
Wojciech Kaliciński <wojciech.kalicinski@jetbrains.com>
2026-04-20 15:02:57 +0200

    [SSoT] Add enhanced Path type to missed String arguments in JS/Wasm
    This is in addition to work that was part of KT-85663

--- compiler/arguments/src/org/jetbrains/kotlin/arguments/description/WasmCompilerArguments.kt
+++ compiler/arguments/src/org/jetbrains/kotlin/arguments/description/WasmCompilerArguments.kt
@@ -245,6 +245,7 @@ val actualWasmArguments by compilerArgumentsLevel(CompilerArgumentsLevelNames.wa
         restrictedToCompilerPhase = KotlinCompilerPhase.BACKEND_COMPILATION
     }
 
+    @OptIn(ExperimentalArgumentApi::class)
     compilerArgument {
         name = "Xir-dce-dump-reachability-info-to-file"
         description = ("Dump reachability information collected about declarations while performing DCE to a file. " +
@@ -253,6 +254,7 @@ val actualWasmArguments by compilerArgumentsLevel(CompilerArgumentsLevelNames.wa
                 "and plain text for all other file types.").asReleaseDependent()
         valueType = StringType.defaultNull
         valueDescription = "<path>".asReleaseDependent()
+        argumentType = PathType.defaultNull
 
         lifecycle(
             introducedVersion = KotlinReleaseVersion.v2_1_20,
@@ -260,6 +262,7 @@ val actualWasmArguments by compilerArgumentsLevel(CompilerArgumentsLevelNames.wa
         restrictedToCompilerPhase = KotlinCompilerPhase.BACKEND_COMPILATION
     }
 
+    @OptIn(ExperimentalArgumentApi::class)
     compilerArgument {
         name = "Xir-dump-declaration-ir-sizes-to-file"
         compilerName = "irDceDumpDeclarationIrSizesToFile"
@@ -269,6 +272,7 @@ val actualWasmArguments by compilerArgumentsLevel(CompilerArgumentsLevelNames.wa
                 "and plain text for all other file types.").asReleaseDependent()
         valueType = StringType.defaultNull
         valueDescription = "<path>".asReleaseDependent()
+        argumentType = PathType.defaultNull
 
         lifecycle(
             introducedVersion = KotlinReleaseVersion.v2_1_20,
