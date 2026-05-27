e51db4c5b550544eafb1067810221d4903a64ff1
Nikita Klimenko <nikita.klimenko@jetbrains.com>
2026-05-27 13:51:44 +0300

    [KDF] Make sure to preserve resolving effects in `cols`
    # Conflicts:
    #	plugins/kotlin-dataframe/testData/box/csDsl/cols.kt

--- plugins/kotlin-dataframe/testData/box/csDsl/cols.kt
+++ plugins/kotlin-dataframe/testData/box/csDsl/cols.kt
@@ -1,3 +1,5 @@
+// CHECK_TYPE_WITH_EXACT
+
 import org.jetbrains.kotlinx.dataframe.*
 import org.jetbrains.kotlinx.dataframe.annotations.*
 import org.jetbrains.kotlinx.dataframe.api.*
@@ -14,5 +16,15 @@ fun box(): String {
         df.select { name },
     )
 
+    val df2 = dataFrameOf(
+        "test" to columnOf("a"),
+        "group" to columnOf(
+            "myCol" to columnOf(1)
+        ),
+    )
+
+    df2.cast<Any>().select { cols("group"["myCol"] named "newName") }.let {
+        checkExactType<Any?>(it.newName[0])
+    }
     return "OK"
 }
