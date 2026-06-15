e4f76d0490a52e0c779547d24bb676f88d470018
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2026-06-15 11:40:55 +0200

    [Analysis API] `analysisApiModules`: drop non-production modules
    Technically, `":analysis:stubs"` is also not the production one,
    but it potentially could be, so better to keet it as it is.
    
    ^KT-86986

--- gradle/compilerModules.gradle.kts
+++ gradle/compilerModules.gradle.kts
@@ -201,6 +201,10 @@ val cliCompilerModules = arrayOf(
     ":kotlin-build-common",
 ).also { extra["cliCompilerModules"] = it }
 
+/**
+ * The array of Analysis API modules that aren't part of [commonCompilerModules] (e.g., `:compiler:psi:psi-api`).
+ * It only covers production modules and is used only as a part of [projectsDependingOnStableStdlib].
+ */
 val analysisApiModules = arrayOf(
     ":analysis:analysis-api",
     ":analysis:analysis-api-fir",
@@ -210,7 +214,6 @@ val analysisApiModules = arrayOf(
     ":analysis:analysis-api-standalone:analysis-api-fir-standalone-base",
     ":analysis:analysis-api-standalone:analysis-api-standalone-base",
     ":analysis:analysis-internal-utils",
-    ":analysis:analysis-test-framework",
     ":analysis:decompiled",
     ":analysis:decompiled:decompiler-js",
     ":analysis:decompiled:decompiler-native",
