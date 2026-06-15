ddb9e319b117e3026ad6a070306ac3f7517b26b9
Radoslaw Panuszewski <radoslaw.panuszewski@jetbrains.com>
2026-06-15 11:38:39 +0200

    [Build] Switch `:native:swift:swift-export-standalone-integration-tests:simple` to `test-inputs-check-v2`
    KTI-3337

--- native/swift/swift-export-standalone-integration-tests/simple/build.gradle.kts
+++ native/swift/swift-export-standalone-integration-tests/simple/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     kotlin("jvm")
     id("java-test-fixtures")
     id("project-tests-convention")
-    id("test-inputs-check")
+    id("test-inputs-check-v2")
 }
 
 description = "A set of integration tests for Swift Export Standalone"
@@ -45,9 +45,6 @@ projectTests {
         allowUnsafe = true, // KT-85212
     ) {
         dependsOn(":kotlin-native:distInvalidateStaleCaches")
-        extensions.configure<TestInputsCheckExtension>("testInputsCheck") {
-            allowFlightRecorder.set(true)
-        }
     }
 
     testGenerator(
