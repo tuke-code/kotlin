6d92fded061bcca46e3a2a9eb03408bd5521133c
Evgeniy.Zhelenskiy <Evgeniy.Zhelenskiy@jetbrains.com>
2026-06-18 15:07:34 +0000

    [FIR, IR, Descriptors] Remove @JvmInline multi-field value classes from the logic
    #KT-84904 #KT-86849

--- plugins/kotlinx-serialization/kotlinx-serialization.backend/src/org/jetbrains/kotlinx/serialization/compiler/backend/ir/SerializerIrGenerator.kt
+++ plugins/kotlinx-serialization/kotlinx-serialization.backend/src/org/jetbrains/kotlinx/serialization/compiler/backend/ir/SerializerIrGenerator.kt
@@ -638,7 +638,7 @@ open class SerializerIrGenerator(
             val serializableDesc = getSerializableClassDescriptorBySerializer(irClass) ?: return
             val generator = when {
                 serializableDesc.isEnumWithLegacyGeneratedSerializer() -> SerializerForEnumsGenerator(irClass, context)
-                serializableDesc.isSingleFieldValueClass(treatFullValueClassesWithOneFieldAsBasic = !context.platform.isJvm()) ->
+                serializableDesc.isSingleFieldValueClass(treatCompatibleFullValueClassesAsInline = !context.platform.isJvm()) ->
                     SerializerForInlineClassGenerator(irClass, context)
                 else -> SerializerIrGenerator(irClass, context)
             }
