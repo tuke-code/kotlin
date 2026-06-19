4803d77f1fed7d8c1311dc6e6bb8982bae09cf4b
Vyacheslav Gerasimov <vyacheslav.gerasimov@jetbrains.com>
2026-06-19 14:01:12 +0200

    Build: Disable flaky `RunConfigurationsTest`
     #KTI-3319

--- repo/codebase-tests/tests/org/jetbrains/kotlin/code/RunConfigurationsTest.kt
+++ repo/codebase-tests/tests/org/jetbrains/kotlin/code/RunConfigurationsTest.kt
@@ -9,6 +9,7 @@ import org.gradle.testkit.runner.GradleRunner
 import org.jetbrains.kotlin.repoTestFixtures.isGitIgnored
 import org.jetbrains.kotlin.testFederation.NightlyTest
 import org.junit.Ignore
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.DynamicTest
 import org.junit.jupiter.api.TestFactory
 import org.w3c.dom.Element
@@ -21,7 +22,7 @@ import kotlin.io.path.inputStream
 import kotlin.io.path.listDirectoryEntries
 import kotlin.streams.asStream
 
-@Ignore
+@Disabled
 @NightlyTest
 class RunConfigurationsTest {
     @TestFactory
