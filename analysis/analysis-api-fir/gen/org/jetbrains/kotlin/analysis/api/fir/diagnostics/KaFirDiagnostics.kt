8fc99bbed39de463c637540c9a832e681879347d
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2026-07-02 06:05:47 +0000

    [FIR] Introduce `KOTLIN_PACKAGE_USAGE` as a frontend diagnostic
    ^KT-85764

--- analysis/analysis-api-fir/gen/org/jetbrains/kotlin/analysis/api/fir/diagnostics/KaFirDiagnostics.kt
+++ analysis/analysis-api-fir/gen/org/jetbrains/kotlin/analysis/api/fir/diagnostics/KaFirDiagnostics.kt
@@ -290,6 +290,10 @@ sealed interface KaFirDiagnostic<PSI : PsiElement> : KaDiagnosticWithPsi<PSI> {
         override val diagnosticClass get() = ParenthesizedPackageQualifierWarning::class
     }
 
+    interface KotlinPackageUsage : KaFirDiagnostic<PsiElement> {
+        override val diagnosticClass get() = KotlinPackageUsage::class
+    }
+
     interface UnsupportedArrayLiteralOutsideOfAnnotationError : KaFirDiagnostic<PsiElement> {
         override val diagnosticClass get() = UnsupportedArrayLiteralOutsideOfAnnotationError::class
     }
