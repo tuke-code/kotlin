9deae990420da6adceecc0eb7f4d97bd126e3857
Radoslaw Panuszewski <radoslaw.panuszewski@jetbrains.com>
2026-07-07 20:59:26 +0200

    Revert "[Native] Update generated sources via `generateMppTargetContainerWithPresets` and `generateMppSourceSetConvention`"
    This reverts commit 9e6bd1911ac0ab53c5c759f748f92dc86c08eab6.

--- libraries/tools/kotlin-gradle-plugin-api/src/common/kotlin/org/jetbrains/kotlin/gradle/dsl/KotlinMultiplatformSourceSetConventions.kt
+++ libraries/tools/kotlin-gradle-plugin-api/src/common/kotlin/org/jetbrains/kotlin/gradle/dsl/KotlinMultiplatformSourceSetConventions.kt
@@ -1512,7 +1512,6 @@ interface KotlinMultiplatformSourceSetConventions {
      *
      * @since 2.0.20
      */
-    @Deprecated(DEPRECATED_TARGET_MESSAGE, level = DeprecationLevel.WARNING)
     val NamedDomainObjectContainer<KotlinSourceSet>.watchosArm32Main: NamedDomainObjectProvider<KotlinSourceSet>
 
     /**
@@ -1536,7 +1535,6 @@ interface KotlinMultiplatformSourceSetConventions {
      *
      * @since 2.0.20
      */
-    @Deprecated(DEPRECATED_TARGET_MESSAGE, level = DeprecationLevel.WARNING)
     val NamedDomainObjectContainer<KotlinSourceSet>.watchosArm32Test: NamedDomainObjectProvider<KotlinSourceSet>
 
     /**
@@ -1870,7 +1868,6 @@ interface KotlinMultiplatformSourceSetConventions {
         configure: LanguageSettingsBuilder.() -> Unit,
     ): Unit = this { languageSettings(configure) }
 }
-
 
 
 
